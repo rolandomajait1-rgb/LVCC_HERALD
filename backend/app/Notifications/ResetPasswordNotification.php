07b3b00af56f2330e86b53f229bc0a66b5118228
rolandomajait1-rgb <rolandomajait1@gmail.com>
2025-11-28 08:20:15 +0800

    Fix ResetPasswordNotification class name conflict

--- backend/app/Notifications/ResetPasswordNotification.php
+++ backend/app/Notifications/ResetPasswordNotification.php
@@ -2,10 +2,10 @@
 
 namespace App\Notifications;
 
-use Illuminate\Auth\Notifications\ResetPassword as ResetPasswordNotification;
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class ResetPasswordNotification extends ResetPasswordNotification
+class ResetPasswordNotification extends ResetPassword
 {
     public function toMail($notifiable)
     {
